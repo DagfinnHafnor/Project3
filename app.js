0ff62819da5882e359c8ee056b10d3587772e4d9
Dagfinn Hafnor <d.hafnor@gmail.com>
2016-05-24 09:47:32 +0200

    newfile

--- app.js
+++ app.js
@@ -1,29 +1,26 @@
 var main = function() {
-  $('.btn').click(function() {
-    var post = $('.status-box').val();
-    $('<li>').text(post).prependTo('.posts');
-    $('.status-box').val('');
-    $('.counter').text('140');
-    $('.btn').addClass('disabled'); 
+  /* Push the body and the nav over by 285px over */
+  $('.icon-menu').click(function() {
+    $('.menu').animate({
+      left: "0px"
+    }, 200);
+
+    $('body').animate({
+      left: "285px"
+    }, 200);
   });
-  
-  $('.status-box').keyup(function() {
-    var postLength = $(this).val().length;
-    var charactersLeft = 140 - postLength;
-    $('.counter').text(charactersLeft);
-  
-    if(charactersLeft < 0) {
-      $('.btn').addClass('disabled'); 
-    }
-    else if(charactersLeft == 140) {
-      $('.btn').addClass('disabled');
-    }
-    else {
-      $('.btn').removeClass('disabled');
-    }
+
+  /* Then push them back */
+  $('.icon-close').click(function() {
+    $('.menu').animate({
+      left: "-285px"
+    }, 200);
+
+    $('body').animate({
+      left: "0px"
+    }, 200);
   });
-  
-  $('.btn').addClass('disabled');
-}
+};
+
 
 $(document).ready(main);
