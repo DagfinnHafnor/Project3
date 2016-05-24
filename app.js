ff6a646e0b15d58eb5dfcd27c9151f4ec9ed020d
Dagfinn Hafnor <d.hafnor@gmail.com>
2016-05-24 14:18:57 +0200

    block

--- app.js
+++ app.js
@@ -1,23 +1,8 @@
 var main = function() {
   /* Push the body and the nav over by 285px over */
   $('.block').click(function() {
-    $('.block').animate({
-      left: "500px"
-    }, 400);
-
-    $('#block1').animate({
-      left: "285px"
-    }, 800);
-  });
-
-  /* Then push them back */
-  $('.bl').click(function() {
-    $('.menu').animate({
-      left: "-285px"
-    }, 200);
-
     $('body').animate({
-      left: "0px"
+      left: "500px"
     }, 200);
   });
 };
